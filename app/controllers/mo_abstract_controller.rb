bd4efa85cde9dca1c81cac4017a7c671a72a0256
Nakamura shinichirou <naka5313@gmail.com>
2010-08-14 15:58:17 +0900

    MO Controller add

---
+++ app/controllers/mo_abstract_controller.rb
@@ -0,0 +1,10 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+class MoAbstractController < ApplicationController
+  trans_sid
+  protect_from_forgery
+  def initialize
+    
+  end
+end
